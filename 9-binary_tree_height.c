894dea6fae7a458f2e604a258cd3c7ccb6230941
faustine-van <faustinemuhayemariya44@gmail.com>
2023-08-30 19:41:05 +0000

    add to header file

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,27 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: pointer to the node to measure the depth
+ * Return: MEASURE OF DEPTH
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	else
+	{
+		int left_height = binary_tree_height(tree->left);
+		int right_height = binary_tree_height(tree->right);
+
+
+		if (left_height >= right_height)
+			return (left_height + 1);
+		else
+			return (right_height + 1);
+
+	}
+
+}
