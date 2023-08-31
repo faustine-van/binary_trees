7c1ee107f54db6e1e9aef942da2d841667449bb3
faustine-van <faustinemuhayemariya44@gmail.com>
2023-08-31 20:39:21 +0000

    updated

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -5,13 +5,15 @@
  * @tree: pointer to the root node of the tree to measure the size
  * Return: the measure of height of tree
  */
-
 size_t binary_tree_size(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
-	int leftSize = binary_tree_size(tree->left);
-	int rightSize = binary_tree_size(tree->right);
 
-	return (leftSize + rightSize + 1);
+	/*int leftSize = binary_tree_size(tree->left);
+	int rightSize = binary_tree_size(tree->right);*/
+
+	return (binary_tree_size(tree->left) +
+			binary_tree_size(tree->right) + 1);
+
 }
