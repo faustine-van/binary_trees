ef1e0e58e99d2f81260af46e3356e89cf85cc124
faustine-van <faustinemuhayemariya44@gmail.com>
2023-08-31 20:41:28 +0000

    updated

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -10,10 +10,6 @@ size_t binary_tree_size(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	/*int leftSize = binary_tree_size(tree->left);
-	int rightSize = binary_tree_size(tree->right);*/
-
 	return (binary_tree_size(tree->left) +
 			binary_tree_size(tree->right) + 1);
-
 }
